39696f94476e05d6a3c8bb024404ba465fe472e0
Oliver Kelly <oliver.philip.kelly@outlook.com>
2025-03-17 16:26:06 +0000

    backend - error middleware

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,15 @@ import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
 
+import { notFound, errorHandler } from './middleware/error.middleware.js'
+
 const port = process.env.PORT || 5000
 
 const app = express()
 
 app.use('/', (req, res) => res.send('Server is Ready'))
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server is listening on port: ${port}`))
